5aa3a3bfe0551ea2c8213078e659fec00164c7d8
O~P~L <stevebickookumu@gmail.com>
2026-08-05 16:19:51 +0300

    Changed Map size

--- client/src/sections/Map.jsx
+++ client/src/sections/Map.jsx
@@ -3,21 +3,17 @@ import Nairobi from "../assets/nairobi.png";
 
 const Map = () => {
   return (
-    <section className="bg-black py-20">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="overflow-hidden rounded-[2rem] border shadow-xl">
-
-          <NavLink to="/map">
-            <img
-              src={Nairobi}
-              alt="Nairobi Map"
-              className="h-auto w-full object-cover transition hover:scale-105"
-            />
-          </NavLink>
-
-        </div>
-      </div>
-    </section>
+<section className="bg-black py-20">
+  <div className="mx-auto max-w-5xl px-6">
+    <NavLink to="/map">
+      <img
+        src={Nairobi}
+        alt="Nairobi Map"
+        className="mx-auto w-[70%] max-w-lg transition duration-500 hover:scale-105"
+      />
+    </NavLink>
+  </div>
+</section>
   );
 };
 
